4c0e41f3adeaff415297ae18c0580c6a34359e47
Diego Luna <diegofcolunalopez@gmail.com>
2020-02-04 14:43:17 -0600

    Webpack Dev Server: Reporte de errores y Cambios en tiempo real

--- src/index.js
+++ src/index.js
@@ -2,4 +2,4 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Helloworld from './components/HelloWorls'
 
-ReactDOM.render(<Helloworld/>,document.getElementById(app));
+ReactDOM.render(<Helloworld/>,document.getElementById('app'));
