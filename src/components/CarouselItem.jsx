1c95790d502243d0ca62ef2f00433dd2a6ebcefe
Diego Luna <diegofcolunalopez@gmail.com>
2020-02-17 08:34:16 -0600

    PropTypes

--- src/components/CarouselItem.jsx
+++ src/components/CarouselItem.jsx
@@ -1,6 +1,7 @@
 /* eslint-disable import/no-unresolved */
 /* eslint-disable jsx-a11y/alt-text */
 import React from 'react';
+import PropTypes from 'prop-types';
 import '../assets/styles/components/CarouselItem.scss';
 
 const CarouselItem = ({ cover, title, year, contentRating, duration }) => (
@@ -18,5 +19,13 @@ const CarouselItem = ({ cover, title, year, contentRating, duration }) => (
     </div>
   </div>
 );
+
+CarouselItem.propTypes = {
+  cover: PropTypes.string,
+  title: PropTypes.string,
+  year: PropTypes.number,
+  contentRating: PropTypes.string,
+  duration: PropTypes.number,
+};
 
 export default CarouselItem;
