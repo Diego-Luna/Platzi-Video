5aedd7340e20a4494dcb3ab23728e8589bcd46a9
Diego Luna <diegofcolunalopez@gmail.com>
2020-03-24 15:00:37 -0600

    Validacion para LogIn LogOut

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,6 +4,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import gravatar from '../utils/gravatar.js';
+import { logoutRequest } from '../actions/index';
 import '../assets/styles/components/Header.scss';
 import userIcon from '../assets/static/icons8-usuario-masculino-26.png';
 import icon from '../assets/static/PlatziVideo.png';
@@ -13,6 +14,10 @@ const Header = ( props ) => {
   const { user } = props;
   const hasUser = Object.keys(user).length > 0;
   
+  const handleLogout = () => {
+    props.logoutRequest({});
+  }
+
   return (
     <header className='header'>
 
@@ -29,22 +34,35 @@ const Header = ( props ) => {
           <p>Perfil</p>
         </div>
         <ul>
-        <li><a href='/'>Cuenta</a></li>
-          <li>
-            <Link to='/login'>
-              Iniciar Sesión
-            </Link>
-          </li>
-        </ul>
-      </div>
-    </header>
+          { hasUser ?
+            <li><a href='/'>{user.name}</a></li> :
+            null
+          }
+
+          { hasUser ?
+          <li><a href="#logout" onClick={handleLogout} >Cerrar Sesión</a></li>
+          :
+            <li>
+              <Link to='/login'>
+                Iniciar Sesión
+              </Link>
+            </li>
+          }
+            
+          </ul>
+        </div>
+      </header>
 );
 };
 
 const mapStateToProps = (state) => {
   return {
     user: state.user
-  }
-}
+  };
+};
 
-export default connect(mapStateToProps,null)(Header);
+const mapDispatchToProps = {
+  logoutRequest,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Header);
