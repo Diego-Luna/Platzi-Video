7766ca158169a1dbe363806e982ee2850af78506
Diego Luna <diegofcolunalopez@gmail.com>
2020-03-25 12:39:00 -0600

    Register con Redux

--- src/containers/Register.jsx
+++ src/containers/Register.jsx
@@ -1,26 +1,38 @@
-/* eslint-disable react/button-has-type */
+// Indico que este componente va a tener estado propio
 import React, { useState } from 'react';
-import '../assets/styles/components/Register.scss';
 import { Link } from 'react-router-dom';
+// 1. Importar modulo para conectar el componente con el store
+import { connect } from 'react-redux';
+// 2. Importar las acciones a ejecutar en el store a través de este componente
+import { registerRequest } from '../actions/index.js';
+import '../assets/styles/components/Register.scss';
 
-const Register = () => {
-
+// 5. El componente ejecuta acciones contra el store, por tanto activo sus props
+const Register = (props) => {
+  
+  // Declaro el estado inicial del componente (la propiedad form representa toda el formulario, por ello le paso un objeto con el valor de incio de cada uno de sus campos)
   const [form, setValues] = useState({
     email: '',
     name: '',
     password: '',
   });
 
+  // Funcion controladora de evento que se dispara cuando uno de los campos de formulario cambia su valor. (onChange)
   const handleInput = (event) => {
+  // Establezco el nuevo estado del componente segun los valores actuales de cada input, pero conservando los anteriores (destructuración)
     setValues({
       ...form,
       [event.target.name]: event.target.value
     })
   }
 
+  // Función controladora de evento que se ejecuta tras dispararse el envio del formulario
   const handleSubmit = (event) => {
     event.preventDefault();
-    console.log(form);
+    // 6. Disparamos la acción registrar el usuario en el store
+    props.registerRequest(form);
+    // Redireccionar a otra ruta.
+    props.history.push('/');
   }
 
   return (
@@ -29,21 +41,21 @@ const Register = () => {
         <h2>Regístrate</h2>
         <form className='login__container--form' onSubmit={handleSubmit} >
           <input 
-            name="name"
+            name='name'
             className='input' 
             type='name' 
             placeholder='Nombre' 
             onChange={handleInput}
           />
           <input 
-            name="email"
+            name='email'
             className='input' 
             type='mail' 
             placeholder='Correo' 
             onChange={handleInput}
           />
           <input 
-            name="password"
+            name='password'
             className='input' 
             type='password' 
             placeholder='Contraseña' 
@@ -59,4 +71,10 @@ const Register = () => {
 );
 }
 
-export default Register;
+// 4. Establecer que acciones llevará a cabo este componente en el store
+const mapDispatchToProps = {
+  registerRequest,
+};
+
+// 3. Conectar el componente con el store
+export default connect(null,mapDispatchToProps)(Register);
